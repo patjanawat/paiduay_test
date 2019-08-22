f8266b645cbb5edaaf254f8d4cdca6ab54515106
Panawat <patjanawat@gmail.com>
2019-08-22 19:17:20 +0700

    Add login, signup component.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import firebase from 'firebase';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let app = '';
+const firebaseConfig = {
+  apiKey: "AIzaSyBD-g-kl9mjJmz7KQ7T8OekrQcC9T-NUCw",
+  authDomain: "pdktest-9211b.firebaseapp.com",
+  databaseURL: "https://pdktest-9211b.firebaseio.com",
+  projectId: "pdktest-9211b",
+  storageBucket: "",
+  messagingSenderId: "768549755599",
+  appId: "1:768549755599:web:b80a10d77e17e684"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+});
